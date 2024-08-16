fbbf313b096e3125e2ab66126b3f8778b1651732
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-16 23:43:00 +0300

    Update CMake things

--- cmake/dynlibutils.cmake
+++ cmake/dynlibutils.cmake
@@ -1,3 +1,7 @@
+# GameData
+# Copyright (C) 2023-2024 Wend4r
+# Licensed under the GPLv3 license. See LICENSE file in the project root for details.
+
 if(NOT DYNLIBUTILS_DIR)
 	message(FATAL_ERROR "DYNLIBUTILS_DIR is empty")
 endif()
@@ -5,14 +9,15 @@ endif()
 set(DYNLIBUTILS_BINARY_DIR "memory_utils")
 
 set(DYNLIBUTILS_INCLUDE_DIR
+	${DYNLIBUTILS_INCLUDE_DIR}
+
 	${DYNLIBUTILS_DIR}/include
 )
-
-set(DYNLIBUTILS_COMPILE_DEFINITIONS)
 
 if(LINUX)
 	set(DYNLIBUTILS_COMPILE_DEFINITIONS
 		${DYNLIBUTILS_COMPILE_DEFINITIONS}
+
 		-D_GLIBCXX_USE_CXX11_ABI=0
 	)
 endif()
--- cmake/platform/linux.cmake
+++ cmake/platform/linux.cmake
@@ -1,18 +1,27 @@
-# Warnings
+# GameData
+# Copyright (C) 2023-2024 Wend4r
+# Licensed under the GPLv3 license. See LICENSE file in the project root for details.
+
 set(PLATFORM_COMPILER_OPTIONS
 	${PLATFORM_COMPILER_OPTIONS}
 
-	-Wall -Wno-uninitialized -Wno-switch -Wno-unused
-	-Wno-conversion-null -Wno-write-strings
-	-Wno-invalid-offsetof -Wno-reorder
-	-Wno-register
+	-Wall
+	-Wno-conversion -Wno-overloaded-virtual -Wno-invalid-offsetof
+	-Wno-non-virtual-dtor -Wno-delete-non-virtual-dtor
+	-Wno-unused -Wno-register
+	-Wno-narrowing
+	-Wno-ordered-compare-function-pointers
 
-	# Others
-	-mfpmath=sse -msse -fno-strict-aliasing
-	-fno-threadsafe-statics -fvisibility=default
+	-mtune=generic -mmmx -msse -msse2 -msse3 -msse4 -msse4.1
+	-fvisibility=default -fPIC
+
+	-fno-strict-aliasing -fno-threadsafe-statics
 )
 
-set(PLATFORM_LINKER_OPTIONS)
+set(PLATFORM_LINKER_OPTIONS
+	${PLATFORM_LINKER_OPTIONS}
+)
+
 set(PLATFORM_COMPILE_DEFINITIONS
 	${PLATFORM_COMPILE_DEFINITIONS}
 
